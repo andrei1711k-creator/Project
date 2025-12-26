cb9102e2da406501a01ac5583c75f9cecd762198
LenzOttenberger <Kirill.Drozd15@gmail.com>
2025-12-26 13:04:48 +0300

    Fixed some text.

--- client/src/api/courses.js
+++ client/src/api/courses.js
@@ -1,24 +1,18 @@
-// src/api/courses.js
 import api from "./axios";
 
-// 🔹 GET /courses с фильтрацией и поиском
 export const getCourses = (params = {}) => 
   api.get("/courses/", { params }).then(res => res.data);
 
-// 🔹 GET /courses/my
 export const getMyCourses = () =>
   api.get("/courses/my").then(res => res.data);
 
-// 🔹 POST /courses/my (создание курса с FormData)
 export const createMyCourse = (formData) =>
   api.post("/courses/my", formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
 
-// 🔹 PUT /courses/my/:id (обновление курса)
 export const updateMyCourse = (id, data) =>
   api.patch(`/courses/my/${id}`, data).then(res => res.data);
 
-// 🔹 DELETE /courses/my/:id
 export const deleteMyCourse = (id) =>
   api.delete(`/courses/my/${id}`).then(res => res.data);
